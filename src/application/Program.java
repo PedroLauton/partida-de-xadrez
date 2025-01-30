d2ea667273c94d8cbdbc1df49eb99a296c85110a
PedroLauton <lautonpedro@gmail.com>
2025-01-29 21:07:28 -0300

    Impressão do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,15 @@
 package application;
 
-public class Program {
+import chess.ChessMatch;
 
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
+		
+		
+	}
 }
