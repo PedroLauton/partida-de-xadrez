6130782c6b6c88a7f7a63aabaab93af1b4a234c1
PedroLauton <lautonpedro@gmail.com>
2025-02-03 18:39:18 -0300

    Movimento das peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,42 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import chess.exceptions.ChessException;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.err.println();
+				System.err.print("Origem: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.err.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e){
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			
+		}
 		
 		
 		
